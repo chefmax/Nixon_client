d660ed67f5b8e3ceb11eed96b9ba573fa277c0f7
Maxim Ostapenko <chefmax7@gmail.com>
2014-10-22 17:14:49 +0400

    Add protection for breakpoints. Check if breakpoint noexists/exist when trying to add/remove breakpoint.

--- command/breakpoint.py
+++ command/breakpoint.py
@@ -4,6 +4,21 @@ from generic_command import *
 
 
 class Breakpoint(GenericCommand):
+    __breakpoints__ = {}
+
+    @staticmethod
+    def _maybe_insert_or_remove_breakpoint(command, addr):
+        if command == 'Z':
+            if addr in Breakpoint.__breakpoints__:
+                return False, "Breakpoint on this address already exists!"
+            Breakpoint.__breakpoints__[addr] = 1
+            return True, "Breakpoint added successfully!"
+        else:
+            if addr not in Breakpoint.__breakpoints__:
+                return False, "Could not delete nonexistent breakpoint!"
+            Breakpoint.__breakpoints__.pop(addr)
+            return True, "Breakpoint removed successfully!"
+
     @property
     def _get_breakpoint_type(self):
         return self._command[1]
@@ -21,11 +36,23 @@ class Breakpoint(GenericCommand):
     @property
     def _generate_result(self):
         self._wait_result = True
+        res_ = Breakpoint._maybe_insert_or_remove_breakpoint(self._command[0], self._get_addr)
+        if not res_[0]:
+            return None, res_[1]
         self._command = self._type + self._get_breakpoint_type + ',' + self._get_addr + ',' + self._get_length
-        return "$" + self._command + '#' + self._get_checksum
+        return "$" + self._command + '#' + self._get_checksum, res_[1]
 
     def execute(self, target):
-        target.send("$g#67")
-        target.recv(1024)
-        target.send(self._generate_result)
-        self._print_result(target)
+        res_ = self._generate_result
+        print(res_[1])
+        if res_[0]:
+            target.send("$g#67")
+            target.recv(1024)
+            target.send(res_[0])
+            self._print_result(target)
+
+    def _print_result(self, target):
+        res_ = target.recv_value(1024)
+        while res_[:2] != "OK":
+            res_ = target.recv_value(1024)
+        print res_
